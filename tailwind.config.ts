299fd555c023e07317ef928137f1d1b79b0db090
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-02-10 01:26:27 +0200

    add images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,42 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "sm-max": { max: "374.99px" },
+      md: "768px",
+      lg: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        gilroy: ["Gilroy", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        ".container": {
+          minWidth: "320px",
+          maxWidth: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          "@screen md": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "1280px",
+          },
+        },
+      });
+    },
+  ],
 };
