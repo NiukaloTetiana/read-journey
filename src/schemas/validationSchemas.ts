a237d2d6fc2deddad7507e1d337a4cf4f10e4913
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-02-12 23:49:47 +0200

    add registerSchema and loginSchema

---
+++ src/schemas/validationSchemas.ts
@@ -0,0 +1,32 @@
+import * as yup from "yup";
+
+import { emailRegExp } from "../constants";
+
+export const registerSchema = yup.object({
+  name: yup
+    .string()
+    .required("Name is required")
+    .min(2, "Min length must be more than 2 chars")
+    .max(32, "Max length must be less than 32 chars"),
+  email: yup
+    .string()
+    .required("Email is required")
+    .email("Enter a valid email")
+    .matches(emailRegExp, "Enter a valid email")
+    .max(64, "Max length must be less than 64 chars"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(7, "Min length must be more than 8 chars")
+    .max(64, "Max length must be less than 64 chars"),
+});
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required("Email is required")
+    .email("Enter a valid email")
+    .matches(emailRegExp, "Enter a valid email")
+    .max(64, "Max length must be less than 64 chars"),
+  password: yup.string().required("Password is required"),
+});
