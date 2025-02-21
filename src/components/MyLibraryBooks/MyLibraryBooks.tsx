832d98104fb2d2e690ce6da5aa4983f435652d26
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-02-21 23:31:08 +0200

    update styles

--- src/components/MyLibraryBooks/MyLibraryBooks.tsx
+++ src/components/MyLibraryBooks/MyLibraryBooks.tsx
@@ -1,3 +1,7 @@
 export const MyLibraryBooks = () => {
-  return <div></div>;
+  return (
+    <div className="w-full rounded-[30px] bg-[#1f1f1f] px-5 py-10 md:px-10">
+      <h2 className="title mb-[34px] md:mb-[28px]">My library</h2>
+    </div>
+  );
 };
--- src/pages/MyLibraryPage.tsx
+++ src/pages/MyLibraryPage.tsx
@@ -3,7 +3,7 @@ import { Dashboard, MyLibraryBooks } from "../components";
 const MyLibraryPage = () => {
   return (
     <div className="mb-10 md:mb-8 lg:mb-[27px]">
-      <div className="container">
+      <div className="container flex flex-col gap-[10px] md:gap-4 lg:flex-row">
         <Dashboard />
         <MyLibraryBooks />
       </div>
