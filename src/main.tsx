e31b318896bc329c41002bd9de5c1268f075932c
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-05-07 22:39:49 +0300

    add redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,23 @@
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 import { App } from "./components";
 
 import "./index.css";
-// import { store } from "./redux";
+import { persistor, store } from "./redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <BrowserRouter>
-      {/* <Provider store={store}> */}
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
-      {/* </Provider> */}
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
       <ToastContainer
         theme="light"
         style={{ zIndex: 99999 }}
---
+++ src/redux/store.ts
@@ -0,0 +1,42 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import { authReducer, AuthState } from "./auth/authSlice";
+
+const authPersistConfig = {
+  key: "auth",
+  version: 1,
+  storage,
+  whitelist: ["refreshToken"],
+};
+
+const persistedAuthReducer = persistReducer<AuthState>(
+  authPersistConfig,
+  authReducer
+);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
