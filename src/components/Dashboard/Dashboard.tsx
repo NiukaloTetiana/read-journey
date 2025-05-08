b9b1f14bab1ad321864f5d18b20b5c709bdf763f
Niukalo Tetiana <tanusha.17121994@gmail.com>
2025-05-08 22:43:03 +0300

    use selectBooksRecommended from store

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -7,10 +7,17 @@ interface IDashboardProps {
 export const Dashboard = ({ children }: IDashboardProps) => {
   const location = useLocation();
   const isLibraryPage = location.pathname === "/library";
+  const isReadingPage = location.pathname === "/reading";
+
+  const gapClasses = isReadingPage
+    ? "gap-10"
+    : isLibraryPage
+      ? "gap-5 md:gap-8 lg:gap-[78px]"
+      : "gap-5 md:gap-8 lg:gap-5";
 
   return (
     <div
-      className={`flex flex-col gap-5 rounded-[30px] bg-[#1f1f1f] p-5 md:flex-row md:gap-8 md:p-8 lg:flex-col lg:p-5 ${isLibraryPage ? "lg:gap-[78px]" : "lg:gap-5"}`}
+      className={`flex flex-col rounded-[30px] bg-[#1f1f1f] p-5 md:flex-row md:p-8 lg:flex-col lg:p-5 ${gapClasses}`}
     >
       {children}
     </div>
--- src/components/RecommendedList/RecommendedList.tsx
+++ src/components/RecommendedList/RecommendedList.tsx
@@ -1,40 +1,12 @@
 import { useLocation } from "react-router-dom";
 
-import { useModal } from "../../hooks";
+import { useAppSelector, useModal } from "../../hooks";
 
 import { AddBookModal, Modal, RecommendedItem } from "../../components";
-
-const books = [
-  {
-    _id: "654fc4d00a563c69b895ef",
-    title: "Lovers of Justice",
-    author: "Yuri Andrukhovych",
-    imageUrl:
-      "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726543/books/1.webp",
-    totalPages: 304,
-    recommend: true,
-  },
-  {
-    _id: "654fc5064f56fe7a8d0e19eb",
-    title: "It doesn't hurt",
-    author: "Kateryna Babkina",
-    imageUrl:
-      "https://res.cloudinary.com/drfvfno3o/image/upload/v1699726598/books/2.png",
-    totalPages: 72,
-    recommend: true,
-  },
-  {
-    _id: "654fe854548f3b992ad6963c",
-    title: "There is land beyond Perekop",
-    author: "Anastasiia Levkova",
-    imageUrl:
-      "https://res.cloudinary.com/drfvfno3o/image/upload/v1699735635/books/654f5eedca10dbb094e5c197_There_is_land_beyond_Perekop.webp",
-    totalPages: 392,
-    recommend: true,
-  },
-];
+import { selectBooksRecommended } from "../../redux";
 
 export const RecommendedList = () => {
+  const books = useAppSelector(selectBooksRecommended);
   const [isAddBookModalOpen, toggleAddBookModal] = useModal();
   const location = useLocation();
   const isHomePage = location.pathname === "/recommended";
